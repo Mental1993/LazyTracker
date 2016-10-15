bf1c112158eab8d35f20e2000f50f178b4ccd7c5
topitsaefera <dimitrismosh@gmail.com>
2016-10-15 20:39:33 +0300

    UI
    Basic UI

--- LazyTracker/app/src/main/java/com/example/mental/lazytracker/MainActivity.java
+++ LazyTracker/app/src/main/java/com/example/mental/lazytracker/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mental.lazytracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
