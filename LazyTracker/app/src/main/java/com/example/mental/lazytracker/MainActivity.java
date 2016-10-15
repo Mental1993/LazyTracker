ba9d5a25ad82291f610a17b97a84bdb38f8bc06c
Dimitris Pechlivanis <sergiokoke16@hotmail.com>
2016-10-15 21:59:18 +0300

    Minor

--- LazyTracker/app/src/main/java/com/example/mental/lazytracker/MainActivity.java
+++ LazyTracker/app/src/main/java/com/example/mental/lazytracker/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.mental.lazytracker;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //Widgets
+    private TextView currLocTextView;
+    private TextView closestMarketTextView;
+    private Button updateLocButton;
+    private Button findClosestButton;
+    private Button showMapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Lock screen for always Portrait mode
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Initialize widgets
+        currLocTextView = (TextView) findViewById(R.id.currLocTextView);
+        closestMarketTextView = (TextView) findViewById(R.id.closestMarketTextView);
+        updateLocButton = (Button) findViewById(R.id.updateLocButton);
+        findClosestButton = (Button) findViewById(R.id.findClosestButton);
+        showMapButton = (Button) findViewById(R.id.showMapButton);
+
     }
 
     @Override
@@ -20,4 +43,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
